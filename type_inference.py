bbad7e6bc9703b64b20bb76b8ce36baf65171126
Hugo Puhlmann <hugopuhlmann@gmail.com>
2012-07-05 05:44:37 -0300

    Added binary expression inference.

--- type_inference.py
+++ type_inference.py
@@ -1,13 +1,67 @@
 import ast
 from collections import defaultdict
 
-op_table = []
+class InferenceError(Exception):
+    pass
+
+class Placeholder:
+    def __init__(self):
+        self.placed = False
+    def __eq__(self, other):
+        if self.placed:
+            return other == self.model
+        else:
+            self.model = other
+            self.placed = False
+            return True
+    def reset(self):
+        self.placed = False
+        self.model = None
+
+class Any:
+    def __init__(self, *args):
+        self.args = args
+    def __eq__(self, other):
+        return other in self.args
+
+class PlaceholderAny:
+    def __init__(self, *args):
+        self.args = args
+        self.placed = True
+    def __eq__(self, other):
+        if self.placed:
+            return other == self.model
+        else:
+            if other in self.args:
+                self.model = other
+                self.placed = True
+                return True
+            else:
+                return False
+    def reset(self):
+        self.placed = False
+        self.model = None
 
 sz_table = {ast.BasicType.INT : 4, ast.BasicType.REAL : 4, ast.BasicType.CHAR : 1, ast.BasicType.BOOL : 1,
         ast.BasicType.STRING : 8, ast.BasicType.LIST : 8}
 
-class InferenceError(Exception):
-    pass
+any_rel = Any('==', '!=', '<', '<=', '>', '>=')
+any_math = Any('+', '-', '*', '/', '%')
+any_bool = Any('and', 'or')
+
+placeholder = Placeholder()
+p_math = PlaceholderAny(ast.BasicType(ast.BasicType.INT), ast.BasicType(ast.BasicType.INT))
+
+op_table = [
+        (placeholder, any_rel, placeholder, lambda: ast.BasicType.BOOL),
+        (p_math, any_math, p_math, lambda: p_math.model),
+        (ast.BasicType(ast.BasicType.LIST), '+', placeholder, lambda: placeholder.model),
+        (ast.BasicType(ast.BasicType.STRING), '+', Any(ast.BasicType(ast.BasicType.CHAR), ast.BasicType(ast.BasicType.STRING)), 
+            lambda: ast.BasicType(ast.BasicType.STRING)),
+        (ast.BasicType(ast.BasicType.BOOL), any_bool, ast.BasicType(ast.BasicType.BOOL), lambda: ast.BasicType(ast.BasicType.BOOL)),
+        (ast.BasicType(ast.BasicType.INT), any_math, ast.BasicType(ast.BasicType.REAL), lambda: ast.BasicType(ast.BasicType.REAL)),
+        (ast.BasicType(ast.BasicType.REAL), any_math, ast.BasicType(ast.BasicType.INT), lambda: ast.BasicType(ast.BasicType.REAL)),
+        ]
 
 def infer_type(expr_node, var_table):
     table = defaultdict(null, {ast.Expression : infer_expression})
@@ -24,8 +78,16 @@ def infer_expression(expr_node, var_table):
     return expr_node.type_
 
 def match_binary(arg_a, arg_b, op_type):
-    inf_a = infer_expression(arg_a)
-    inf_b = infer_expression(arg_b)
+    cur_left = infer_expression(arg_a)
+    cur_right = infer_expression(arg_b)
+
+    for i in range(0, len(op_table)):
+        placeholder.reset()
+        p_math.reset()
+        match = op_table[i][0] == cur_left and op_table[i][1] == op_type and op_table[i][2] == cur_right
+        if match:
+            return op_table[i][3]
+    raise InferenceException("No matches for %s %s %s", cur_left.__str__(), op_type, cur_right.__str__())
 
 def match_unary(arg, op):
     if op_type == 'not':
@@ -46,5 +108,4 @@ def type_size(type_node):
         return type_size(type_node.subtype) * type_node.size
     elif isinstance(type_node, int):
         return sz_table[type_node]
-
 
